b99c49a0a1519ce4ec8c6a19ab515473fbe9f2b3
carbon <79378332419@yandex.ru>
2022-03-26 19:26:55 +0500

    123

--- laba1Almaz/laba1Almaz.cpp
+++ laba1Almaz/laba1Almaz.cpp
@@ -1,7 +1,14 @@
-﻿#include <GL/freeglut.h>
+﻿#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/vec3.hpp>
+GLuint VBO;
 static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glutSwapBuffers();
 }
 int main(int argc, char* argv[]) {
@@ -11,7 +18,22 @@ int main(int argc, char* argv[]) {
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Tutorial 01");
 	glutDisplayFunc(RenderSceneCB);
-	glClearColor(0.0f, 0.5f, 0.0f, 0.0f);
-	glutMainLoop();
 
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+	glm::vec3 Vertices[3];
+	Vertices[0] = glm::vec3(1.0f, 1.0f, 0.0f);
+	Vertices[1] = glm::vec3(-1.0f, 1.0f, 0.0f);
+	Vertices[2] = glm::vec3(0.0f,-1.0f, 0.0f);
+	
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	
+	glutMainLoop();
+		
 }
